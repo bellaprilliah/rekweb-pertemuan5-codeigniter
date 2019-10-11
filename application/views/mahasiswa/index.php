8f409b8d33d472daa91c9079cbab3f8b8f480533
Bella Aprillia H <bellaprilliah@gmail.com>
2019-10-11 07:47:31 +0700

    rekweb-pertemuan5-codeigniter

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+    <h3>Daftar Mahasiswa</h3>
+    <?php foreach ($mhs as $m) : ?>
+    <ul>
+    <li><?= $m ['nrp'];?></li>
+    <li><?= $m ['nama'];?></li>
+    <li><?= $m ['email'];?></li>
+    <li><?= $m ['jurusan'];?></li>
+    </ul>
+<?php endforeach; ?>
+</body>
+</html>
